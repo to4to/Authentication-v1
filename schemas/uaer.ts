cfadadcf2bdbdd4772998b75744ab344bd865564
to4to <justjustifying@outlook.com>
2023-11-12 23:56:25 +0530

    Schema Completed

--- schemas/uaer.ts
+++ schemas/uaer.ts
@@ -1,1 +1,19 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+
+    {
+
+        name: String,
+        email: String,
+        authentication: {
+
+            password: { type: String },
+            access_tpoken: {type: String }
+        }
+
+
+    }
+);
+
+
